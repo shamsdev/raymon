51a0afa123acc6b1aaa75fb704062cd5e88fd562
Mohammad Shams <mamad.sh96@gmail.com>
2020-04-21 21:15:52 +0430

    fix test scripts for smoke test

--- test/test.js
+++ test/test.js
@@ -1,3 +1,10 @@
+'use strict';
+
 const {RaymonServer} = require('../lib/');
 const server = new RaymonServer();
 server.start();
+
+setTimeout(() => {
+    server.stop();
+    process.exit(0);
+}, 1000);
