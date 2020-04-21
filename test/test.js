5ea3082860024b9b7c3cec3986fd6b71857ebfe1
Mohammad Shams <mamad.sh96@gmail.com>
2020-04-21 13:46:10 +0430

    put class vars in constructor

--- test/test.js
+++ test/test.js
@@ -2,7 +2,6 @@ const {RaymonServer} = require('../lib/');
 
 const server = new RaymonServer();
 server.start();
-console.log(RaymonServer.SocketType.TCP);
 server.addRequestHandler("hello", (socketType, user, packet) => {
     console.log(`user ${user.id} sent me ${packet.payload}`);
     server.send(RaymonServer.SocketType.TCP, user, "hello_back", {hello: "back"})
