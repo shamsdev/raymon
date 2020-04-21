a3543f766ed9b6befbfbb65516a4c064cab90e9d
Mohammad Shams <mamad.sh96@gmail.com>
2020-04-21 17:55:19 +0430

    web socket handshake

--- test/test.js
+++ test/test.js
@@ -1,8 +1,3 @@
 const {RaymonServer} = require('../lib/');
-
 const server = new RaymonServer();
 server.start();
-server.addRequestHandler("hello", (socketType, user, packet) => {
-    console.log(`user ${user.id} sent me ${packet.payload}`);
-    server.send(RaymonServer.SocketType.TCP, user, "hello_back", {hello: "back"})
-});
