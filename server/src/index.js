00e98e0dbae6e6340fd222048ce65f75c9d4ad0e
Mohammad Shams <mamad.sh96@gmail.com>
2020-04-21 13:26:13 +0430

    improve folder structure

--- server/src/index.js
+++
@@ -1,2 +0,0 @@
-module.exports.RaymonServer = require('./server/server');
-
--- test/test.js
+++ test/test.js
@@ -1,9 +1,9 @@
-const {RaymonServer} = require('../src/index');
+const {RaymonServer} = require('../lib/');
 
 const server = new RaymonServer();
 server.start();
 
 server.addRequestHandler("hello", (socketType, user, packet) => {
     console.log(`user ${user.id} sent me ${packet.payload}`);
-    server.send(RaymonServer.SocketType.UDP, user, "hello_back", {hello: "back"})
+    server.send(RaymonServer.SocketType.TCP, user, "hello_back", {hello: "back"})
 });
