66363f95740e3d14bd7ab486c68dd2d7f416000f
Mohammad Shams <mamad.sh96@gmail.com>
2020-04-24 03:05:17 +0430

    minor bug fix on rooms

--- test/test.js
+++ test/test.js
@@ -4,6 +4,12 @@ const {RaymonServer} = require('../lib/');
 const server = new RaymonServer();
 server.start();
 
+let room = server.createRoom();
+
+server.addRequestHandler("hey",(socket,user,packet)=>{
+
+});
+
 setTimeout(() => {
     server.stop();
     process.exit(0);
