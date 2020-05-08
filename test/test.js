3a6b897e5699b152df248efeef1c919ba5bbf87b
Mohammad Shams <mamad.sh96@gmail.com>
2020-05-08 19:18:01 +0430

    improve send structure, add packet type

--- test/test.js
+++ test/test.js
@@ -8,11 +8,17 @@ let room = server.createRoom();
 room.sendAll();
 
 server.addRequestHandler("hey", (args) => {
+    server.sendMessage(args.user, "hey", {});
+});
 
+server.addEventHandler(RaymonServer.EventHandler.OnUserConnect, (args) => {
+    console.log('user connect: ' + args.user.id);
+    server.sendMessage(args.user, "hey", {});
 });
 
 server.addEventHandler(RaymonServer.EventHandler.OnUserDisconnect, (args) => {
-
+    console.log('user disconnected: ' + args.user.id);
+    console.log('session time: ' + args.user.sessionTime + 's');
 });
 
 setTimeout(() => {
